668f12166d52c58ee783588e6fb5ed441642b683
Adam Pearce <adam.r.pearce@gmail.com>
2014-12-16 15:50:05 -0500

    reverts main.js

--- main.js
+++ main.js
@@ -1,95 +1,64 @@
-/**
- * Module dependencies.
- */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , hike = require('./routes/hike')
-  , http = require('http')
-  , path = require('path')
-  , mysql = require('mysql')
-  , async = require('async');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var hike = require('./routes/hike');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-//  app.use(express.static(path.join(__dirname, 'public')));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/hike', hike.index);
+
+app.post('/add_hike', hike.add_hike)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.configure('development', function() {
-  console.log('Using development settings.');
-  app.set('connection', mysql.createConnection({
-    host: '',
-    user: '',
-    port: '',
-    password: ''}));
-  app.use(express.errorHandler());
-});
+// error handlers
 
-app.configure('production', function() {
-  console.log('Using production settings.');
-  app.set('connection', mysql.createConnection({
-    host: process.env.RDS_HOSTNAME,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    port: process.env.RDS_PORT}));
-});
-
-function init() {
-  app.get('/', routes.index);
-  app.get('/users', user.list);
-  app.get('/hikes', hike.index);
-  app.post('/add_hike', hike.add_hike);
-
-  http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
-  });
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
-var client = app.get('connection');
-async.series([
-  function connect(callback) {
-    client.connect(callback);
-  },
-  function clear(callback) {
-    client.query('DROP DATABASE IF EXISTS mynode_db', callback);
-  },
-  function create_db(callback) {
-    client.query('CREATE DATABASE mynode_db', callback);
-  },
-  function use_db(callback) {
-    client.query('USE mynode_db', callback);
-  },
-  function create_table(callback) {
-     client.query('CREATE TABLE HIKES (' +
-                         'ID VARCHAR(40), ' +
-                         'HIKE_DATE DATE, ' +
-                         'NAME VARCHAR(40), ' +
-                         'DISTANCE VARCHAR(40), ' +
-                         'LOCATION VARCHAR(40), ' +
-                         'WEATHER VARCHAR(40), ' +
-                         'PRIMARY KEY(ID))', callback);
-  },
-  function insert_default(callback) {
-    var hike = {HIKE_DATE: new Date(), NAME: 'Hazard Stevens',
-          LOCATION: 'Mt Rainier', DISTANCE: '4,027m vertical', WEATHER:'Bad'};
-    client.query('INSERT INTO HIKES set ?', hike, callback);
-  }
-], function (err, results) {
-  if (err) {
-    console.log('Exception initializing database.');
-    throw err;
-  } else {
-    console.log('Database initialization complete.');
-    init();
-  }
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
+
+module.exports = app;
